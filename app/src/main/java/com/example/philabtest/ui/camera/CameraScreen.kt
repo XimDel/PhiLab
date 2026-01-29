dc42f7e32b72d98afb34a5cd1c00b70d248ca47d
Ximena Delgado <xim.delgado@outlook.com>
2026-01-29 15:03:41 -0500

    Camerascreen.kt

---
+++ app/src/main/java/com/example/philabtest/ui/camera/CameraScreen.kt
@@ -0,0 +1,9 @@
+package com.example.philabtest.ui.camera
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CameraScreen() {
+    Text("Camera Screen (próximo: CameraX)")
+}
