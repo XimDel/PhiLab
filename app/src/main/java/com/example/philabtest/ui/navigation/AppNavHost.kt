d1a7ed5ccbbaab744d7b5cceacc0ddd8a9bff3ba
Ximena Delgado <xim.delgado@outlook.com>
2026-01-29 14:52:27 -0500

    AppNavHost.kt mapa de navegación que conecta esas rutas con sus pantallas y controla a dónde ir cuando el usuario presiona un botón.

---
+++ app/src/main/java/com/example/philabtest/ui/navigation/AppNavHost.kt
@@ -0,0 +1,27 @@
+//mapa de navegación que conecta esas rutas con sus pantallas y controla a dónde ir cuando el usuario presiona un botón.
+
+package com.example.philabtest.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.philabtest.ui.home.HomeScreen
+import com.example.philabtest.ui.home.HomeViewModel
+
+
+@Composable
+fun AppNavHost(homeViewModel: HomeViewModel) {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = Routes.HOME) {
+        composable(Routes.HOME) {
+            HomeScreen(
+                viewModel = homeViewModel,
+                onStartExperiment = { navController.navigate(Routes.CAMERA) },
+                onOpenHistory = { navController.navigate(Routes.HISTORY) }
+            )
+        }
+
+    }
+}
