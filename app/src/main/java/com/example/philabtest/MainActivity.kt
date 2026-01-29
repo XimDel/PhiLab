b59fa0fea0e51e05adefd0527b3c28d64bea31ea
Ximena Delgado <xim.delgado@outlook.com>
2026-01-29 14:36:04 -0500

    MainActivity.kt iniciado

--- app/src/main/java/com/example/philabtest/MainActivity.kt
+++ app/src/main/java/com/example/philabtest/MainActivity.kt
@@ -3,44 +3,26 @@ package com.example.philabtest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.activity.viewModels
+import com.example.philabtest.ui.home.HomeScreen
+import com.example.philabtest.ui.home.HomeViewModel
 import com.example.philabtest.ui.theme.PhiLabTestTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val homeViewModel: HomeViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             PhiLabTestTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                HomeScreen(
+                    viewModel = homeViewModel,
+                    onStartExperiment = { /* TODO: ir a pantalla cámara */ },
+                    onOpenHistory = { /* TODO: ir a historial */ }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PhiLabTestTheme {
-        Greeting("Android")
     }
 }
