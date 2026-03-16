edd4369a3b9d8ad23b25b1e6b4703bf733699632
Ximena Delgado <xim.delgado@outlook.com>
2026-03-16 13:32:00 -0500

    Inicialización de OpenCV en MainActivity.kt

--- app/src/main/java/com/example/philab/MainActivity.kt
+++ app/src/main/java/com/example/philab/MainActivity.kt
@@ -5,11 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.philab.ui.navigation.AppNavHost
 import com.example.philab.ui.theme.PhiLabTheme
+import org.opencv.android.OpenCVLoader
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        OpenCVLoader.initLocal()
 
         setContent {
             PhiLabTheme {
